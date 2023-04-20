d6ae89355f274ee9b681426482e8b177079f8997
Roc <>
2023-04-20 15:40:18 +0800

    test bilibili 首页信息流广告移除

--- bilibili/index.js
+++ bilibili/index.js
@@ -1,24 +1,45 @@
 const version = "1.0.0";
 const proxy_name = " 哔哩哔哩 AdGuard";
-console.log(`${proxy_name}: ${version}`);
+console.info(`${proxy_name}: ${version}`);
 
 let body = $response.body;
 const url = $request.url;
 
+const year = 60 * 60 * 24 * 365;
+
 // 开屏
-const bl_splash = /\/splash\/show/.test(url);
+const bl_splash = /\/splash\/(show|list)/.test(url);
 // 首页信息流
 const bl_feed = /\/feed\/index/.test(url);
 
 // 开屏
 if (bl_splash) {
   let obj = JSON.parse(body);
-  if (obj.data && obj.data.show) {
-    // 9999/12/31 00:00:00
-    // const stime = 253402185600;
-    // const etime = 253402185600;
-    obj.data.show = [];
+  if (obj.data) {
+    if (obj.data.show) {
+      // 9999/12/31 00:00:00
+      // const stime = 253402185600;
+      // const etime = 253402185600;
+      obj.data.show = [];
+    }
+    if (obj.data.list) {
+      obj.data.list = [];
+    }
+
+    if (obj.data.min_interval) {
+      obj.data.min_interval = year;
+    }
+
+    if (obj.data.pull_interval) {
+      obj.data.pull_interval = year;
+    }
+
+    if (obj.data.max_time) {
+      obj.data.max_time = 0;
+    }
+
   }
+  
   body = JSON.stringify(obj);
 }
 

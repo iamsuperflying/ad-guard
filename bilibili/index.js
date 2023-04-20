7504997ea2a1999f73e2f6ed7ef2dd1ccd3dcddd
Roc <>
2023-04-20 14:48:11 +0800

    test bilibili 首页信息流广告移除

--- bilibili/index.js
+++ bilibili/index.js
@@ -5,8 +5,12 @@ console.log(`${proxy_name}: ${version}`);
 let body = $response.body;
 const url = $request.url;
 
-const bl_splash = /\/x\/v2\/splash\/show/.test(url);
+// 开屏
+const bl_splash = /\/splash\/show/.test(url);
+// 首页信息流
+const bl_feed = /\/feed\/index/.test(url);
 
+// 开屏
 if (bl_splash) {
   let obj = JSON.parse(body);
   if (obj.data && obj.data.show) {
@@ -14,6 +18,15 @@ if (bl_splash) {
     // const stime = 253402185600;
     // const etime = 253402185600;
     obj.data.show = [];
+  }
+  body = JSON.stringify(obj);
+}
+
+// 首页信息流
+if (bl_feed) {
+  let obj = JSON.parse(body);
+  if (obj.data && obj.data.items) {
+    obj.data.items = obj.data.items.filter((item) => item.ad_info == null);
   }
   body = JSON.stringify(obj);
 }
